f2808663637fc74ddb4087b4458f11809f7dff7e
WAlexandreW <35145996+WAlexandreW@users.noreply.github.com>
2020-10-11 23:55:55 +0200

    Update Regression_Standard.py

--- Regression_Standard.py
+++ Regression_Standard.py
@@ -4,3 +4,9 @@ import os
 checkpoint_root = r"D:\Projets"
 wd=os.chdir(checkpoint_root)
 
+# It will test different regressions algorithms:
+#Multiple Linear Regressions
+#Polynomial regression
+#ANN
+#XGBOOST
+
