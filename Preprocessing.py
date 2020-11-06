aaec76d5df695c99cf0d0e98ec9124008943a6ba
WAlexandreW <35145996+WAlexandreW@users.noreply.github.com>
2020-11-06 18:19:37 +0100

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 #drop_first set to true to avoid the dummy trap
 var = pd.get_dummies(df["var1"], drop_first = True)
+
+
+#2) Scaling: either MinMaxscaler or StandargScaler
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+df = scaler.fit_transform(df)
